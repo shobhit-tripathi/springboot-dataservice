166f9d6a5c3cf756069f1690ea63debdb2f8cd96
Shobhit Tripathi <tripathishobhit1990@gmail.com>
2024-12-30 16:43:35 +0530

    Initial commit for Spring Boot Data services (demo) application

---
+++ src/test/java/com/dev/demoproject/dataservice/DataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.demoproject.dataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
